580a416fca6e1cdb6592c06922bf729aab005db2
versiw <stao.ding@qq.com>
2025-11-11 10:28:26 +0800

    chore: 初始化模板

---
+++ apps/web/postcss.config.mjs
@@ -0,0 +1,3 @@
+import { postcssConfig } from '@next-template/config'
+
+export default postcssConfig
---
+++ packages/config/index.ts
@@ -0,0 +1,9 @@
+import postcssConfig from './postcss.config.mjs'
+
+const allConfigs = {
+  postcss: postcssConfig
+}
+
+export default allConfigs
+
+export { postcssConfig }
